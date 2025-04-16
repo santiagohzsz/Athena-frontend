1033e19acacd5ab4f5ad0ee571932970a87b029d
stikxe <athenadirection.mx@gmail.com>
2025-04-16 14:42:58 -0600

    cambios navbar

--- js/script.js
+++ js/script.js
@@ -74,6 +74,13 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
+  const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
   // 📥 Desde el showcase
   if (showcaseBtn && showcaseInput) {
     showcaseBtn.addEventListener("click", () => {
