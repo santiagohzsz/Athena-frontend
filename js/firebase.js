7a383b88fc78927103b0b6500f0f9ad49db1e4b2
santiagohzsz <santiagohzszmex@gmail.com>
2025-04-20 15:18:34 -0600

    eliminacion

--- js/firebase.js
+++ js/firebase.js
@@ -1,15 +1,1 @@
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB5kJ-y_T4FatBx8cGjc8UX6a-gEvtEsuA",
-  authDomain: "proyecto-athena-24d30.firebaseapp.com",
-  projectId: "proyecto-athena-24d30",
-  storageBucket: "proyecto-athena-24d30.firebasestorage.app",
-  messagingSenderId: "1068508593232",
-  appId: "1:1068508593232:web:ca3cab5378c3fb20c2e555",
-  measurementId: "G-BFRXZENRH6"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
