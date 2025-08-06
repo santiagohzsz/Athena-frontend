ceb60a9cebe7f67b2b14b6540a7a6f1f85ffb201
Mathias Rendon <mathiassaid7@gmail.com>
2025-08-06 18:03:21 -0500

    style: make responsive the basic layout (Header and Footer)

--- src/common/layout/Footer.jsx
+++ src/common/layout/Footer.jsx
@@ -3,10 +3,10 @@ import { Github, Linkedin, Twitter, Youtube } from "lucide-react"
 function Footer() {
     return (
         <>
-            <div className="footer w-full py-20 flex justify-center items-center text text-white bg-neutral-900 pl-22 pr-8">
-                <div className="athena-project w-[40%] flex flex-col gap-2">
+            <div className="footer w-full py-20 flex justify-center items-center text text-white bg-neutral-900 pl-22 pr-8 max-lg:pl-10 max-md:flex-col-reverse gap-12">
+                <div className="athena-project w-[40%] flex flex-col gap-2 max-md:w-max max-md:items-center">
                     <img src="athena-project.svg" alt="Athena Project" />
-                    <p className="text-neutral-400">Innovando el futuro con inteligencia artificial.</p>
+                    <p className="text-neutral-400 max-md:text-center">Innovando el futuro con inteligencia artificial.</p>
                     <ul className="flex gap-4 text-neutral-400 mt-4">
                         <li className="hover:text-white cursor-pointer"><Twitter /></li>
                         <li className="hover:text-white cursor-pointer"><Linkedin /></li>
@@ -14,8 +14,8 @@ function Footer() {
                         <li className="hover:text-white cursor-pointer"><Youtube /></li>
                     </ul>
                 </div>
-                <div className="second-grid grid grid-cols-3 w-[60%]">
-                    <div className="products flex flex-col gap-2">
+                <div className="second-grid grid grid-cols-3 w-[60%] max-md:grid-cols-1 gap-6">
+                    <div className="products flex flex-col gap-2 max-md:items-center">
                         <h4 className="font-semibold">Productos</h4>
                         <ul className="flex flex-col gap-2 text-neutral-400">
                             <li><a href="#">Athena Innovis</a></li>
@@ -24,7 +24,7 @@ function Footer() {
                             <li><a href="#">AthenaBiz</a></li>
                         </ul>
                     </div>
-                    <div className="resources flex flex-col gap-2">
+                    <div className="resources flex flex-col gap-2 max-md:items-center">
                         <h4 className="font-semibold">Resources</h4>
                         <ul className="flex flex-col gap-2 text-neutral-400">
                             <li><a href="#">Documentación</a></li>
@@ -33,7 +33,7 @@ function Footer() {
                             <li><a href="#">Soporte</a></li>
                         </ul>
                     </div>
-                    <div className="company flex flex-col gap-2">
+                    <div className="company flex flex-col gap-2 max-md:items-center">
                         <h4 className="font-semibold">Compañía</h4>
                         <ul className="flex flex-col gap-2 text-neutral-400">
                             <li><a href="#">Nosotros</a></li>
@@ -45,8 +45,8 @@ function Footer() {
                 </div>
             </div>
             <div className="copyright flex justify-center items-center text-neutral-400 bg-neutral-900 border-t border-t-neutral-800 py-6 flex-col gap-4">
-                <p>&copy; 2023 Athena Project. Todos los derechos reservados</p>
-                <p>Made By <a href="https://github.com/JunLovin" target="_blank" className="underline">JunLovin</a> - from <a href="https://syntax-world-workspace.vercel.app" target="_blank" className="underline">Syntax World</a> with ♥️</p>
+                <p className="text-sm max-sm:text-center">&copy; 2023 Athena Project. Todos los derechos reservados</p>
+                <p className="text-sm max-sm:text-center">Made By <a href="https://github.com/JunLovin" target="_blank" className="underline">JunLovin</a> - from <a href="https://syntax-world-workspace.vercel.app" target="_blank" className="underline">Syntax World</a> with ♥️</p>
             </div>
         </>
     )
--- src/common/layout/Header.jsx
+++ src/common/layout/Header.jsx
@@ -1,21 +1,74 @@
-import { Contact, GraduationCap, Home, LogIn, Map } from "lucide-react"
+import { useState, useEffect } from 'react'
+import { Contact, GraduationCap, Home, LogIn, Map, Menu, X } from "lucide-react"
 import { Link } from "react-router-dom"
 
 function Header() {
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 820)
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen)
+    }
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 820)
+        }
+
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+
     return (
         <>
-        <div className="header-container w-full h-full flex justify-between items-center px-8">
-            <div className="header-left">
-                <h2 className="text-sky-500 font-bold text-2xl">Athena Sphere</h2>
+            <div className="header-container w-full h-full flex justify-between items-center px-8 max-sm:px-4">
+                <div className="header-left">
+                    <h2 className="text-sky-500 font-bold text-2xl">Athena Sphere</h2>
+                </div>
+                <div className="header-right flex gap-8 items-center text-sm">
+                    {!isMobile ? (
+                        <>
+                            <Link to="/" className="font-semibold text-slate-800 flex items-center gap-1">Inicio <Home className="size-4" /> </Link>
+                            <Link to="/" className="font-semibold text-slate-800 flex items-center gap-1">AthenaEdu <GraduationCap className="size-4" /> </Link>
+                            <Link to="/" className="font-semibold text-slate-800 flex items-center gap-1">Innovis <Map className="size-4" /></Link>
+                            <Link to="/" className="font-semibold text-slate-800 flex items-center gap-1">Contacto <Contact className="size-4" /> </Link>
+                            <button className="px-4 py-2 rounded-md bg-sky-600 text-white font-semibold hover:bg-sky-700 active:bg-sky-600 transition-colors duration-200 cursor-pointer flex items-center justify-center gap-2">Iniciar Sesión <LogIn className="size-4" /> </button>
+                        </>
+                    ) : (
+                        <>
+                            <button onClick={toggleMobileMenu}>
+                                <Menu />
+                            </button>
+                        </>
+                    )}
+                </div>
             </div>
-            <div className="header-right flex gap-8 items-center text-sm">
-                <Link to="/" className="font-semibold text-slate-800 flex items-center gap-1">Inicio <Home className="size-4" /> </Link>
-                <Link to="/" className="font-semibold text-slate-800 flex items-center gap-1">AthenaEdu <GraduationCap className="size-4" /> </Link>
-                <Link to="/" className="font-semibold text-slate-800 flex items-center gap-1">Innovis <Map className="size-4" /></Link>
-                <Link to="/" className="font-semibold text-slate-800 flex items-center gap-1">Contacto <Contact className="size-4" /> </Link>
-                <button className="px-4 py-2 rounded-md bg-sky-600 text-white font-semibold hover:bg-sky-700 active:bg-sky-600 transition-colors duration-200 cursor-pointer flex items-center justify-center gap-2">Iniciar Sesión <LogIn className="size-4" /> </button>
-            </div>
-        </div>
+
+            {isMobileMenuOpen && (
+                <>
+                    <div className="mobile-menu-container w-full h-[40dvh] fixed top-0 bg-white z-100 rounded-br-xl rounded-bl-xl flex flex-col p-4">
+                        <div className="menu-top flex justify-between items-center">
+                            <div className="logo">
+                                <h2 className="text-sky-500 font-bold text-2xl">Athena Sphere</h2>
+                            </div>
+                            <div className="close-menu-btn w-max self-end">
+                                <button onClick={toggleMobileMenu}>
+                                    <X />
+                                </button>
+                            </div>
+                        </div>
+                        <div className="links-list flex flex-col gap-4 h-full justify-evenly">
+                            <Link to="/" className="font-semibold text-slate-800 flex items-center gap-1">Inicio <Home className="size-4" /> </Link>
+                            <Link to="/" className="font-semibold text-slate-800 flex items-center gap-1">AthenaEdu <GraduationCap className="size-4" /> </Link>
+                            <Link to="/" className="font-semibold text-slate-800 flex items-center gap-1">Innovis <Map className="size-4" /></Link>
+                            <Link to="/" className="font-semibold text-slate-800 flex items-center gap-1">Contacto <Contact className="size-4" /> </Link>
+                        </div>
+                    </div>
+                </>
+            )}
         </>
     )
 }
